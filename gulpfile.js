8880bb455e3a8879c713655fb935f2f86a7aaf19
maj-mkl <miguel.alcaraz.sopra@gmail.com>
2015-11-23 17:51:04 +0100

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require("gulp-rename");
 
 gulp.task('sass', function () {
 	return sass('proof-of-concept/gsites-proof-of-concept.scss')
-	.pipe(gulp.dest('proof-of-concept'));
+	.pipe(gulp.dest('proof-of-concept'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifyCss())
+	.pipe(gulp.dest('proof-of-concept'))
 });
 
 gulp.task('default', function() {
-	console.log('Gulp!');
+	console.log('Gulp!')
 });
